33b639a68a8e98dc805e00cc1e406b4118441b9d
Nick Schutt <nicholas.schutt@nxp.com>
2019-05-09 16:30:15 +0200

    use "jobs show" instead of "jobs list" ... this guarantees to read the data for the specific jobs; "jobs list" provides the list of the last jobs present in the queue

--- test/lava/get_lava_status.sh
+++ test/lava/get_lava_status.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-lavacli --identity $1 jobs list --limit 100 | grep -f LAVA_JOBS
+#lavacli --identity $1 jobs list --limit 100 | grep -f LAVA_JOBS
+
+jobs=`cat LAVA_JOBS`
+
+for jobid in $jobs
+do
+    lavacli --identity $1 jobs show ${jobid} | awk '
+            { split($0,p," *: "); vars[p[1]] = p[2]; }
+            END { print vars["id"]": "vars["state"]","vars["Health"]" ["vars["submitter"]"] " vars["description"]" - "vars["device"]}
+            '
+done
 
