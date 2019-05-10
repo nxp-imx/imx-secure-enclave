dc234ad9a27227e7f11168529018200301d60e02
Nick Schutt <nicholas.schutt@nxp.com>
2019-05-10 13:15:40 +0200

    SHE-78: Get a specific list of lava jobs and their current status using show, instead of list

--- test/lava/get_lava_logs.sh
+++ test/lava/get_lava_logs.sh
@@ -5,9 +5,8 @@ jobs=`cat LAVA_JOBS`
 for jobid in $jobs
 do
     echo "-----------------------------------------------------------------"
-    echo ${jobid}
+    echo ${jobid} - http://lava-master.sw.nxp.com/scheduler/job/${jobid}
     echo "-----------------------------------------------------------------"
-    lavacli --identity $1 jobs logs ${jobid} > ${jobid}.log
-    sed -n "/<LAVA_SIGNAL_STARTRUN /,/<LAVA_SIGNAL_ENDRUN /p" ${jobid}.log
+    lavacli --identity $1 jobs logs ${jobid} | tee ${jobid}.log | sed -n "/<LAVA_SIGNAL_STARTRUN /,/<LAVA_SIGNAL_ENDRUN /p"
 done
 
--- test/lava/get_lava_status.sh
+++ test/lava/get_lava_status.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+#
+# Get a specific list of lava jobs and their current status
+#
+# This will work at any time, as long as the jobs are still in the lava database.
+#
+# If no ids are specified, read the ids from a file, LAVA_JOBS
+#
 
-#lavacli --identity $1 jobs list --limit 100 | grep -f LAVA_JOBS
+identity=$1
+shift
 
-jobs=`cat LAVA_JOBS`
+if [ "0" != $# ]
+then
+    jobs=$@
+else
+    jobs=`cat LAVA_JOBS`
+fi
 
 for jobid in $jobs
 do
-    lavacli --identity $1 jobs show ${jobid} | awk '
+    lavacli --identity $identity jobs show ${jobid} | awk '
             { split($0,p," *: "); vars[p[1]] = p[2]; }
             END { print vars["id"]": "vars["state"]","vars["Health"]" ["vars["submitter"]"] " vars["description"]" - "vars["device"]}
             '
