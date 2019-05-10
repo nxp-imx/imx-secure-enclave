6a5cc325cec54a5bc123465ce233bc5c542b5503
Nick Schutt <nicholas.schutt@nxp.com>
2019-05-10 13:16:06 +0200

    SHE-78: prepare a test report

--- test/lava/prepare_test_report.sh
+++ test/lava/prepare_test_report.sh
@@ -4,24 +4,31 @@ exec_dir=`dirname $0 | xargs realpath`
 
 data=`${exec_dir}/accumulate_lava_results.sh $@ | grep -P '^\S+ <|TEST RESULT' | grep -vP '<test>|<\/test>|<LAVA_SIGNAL' | grep -oP '<.+|TEST RESULT.+'`
 
-#echo "$data"
-#requirements=[]
-
-fields=[]
-line=[]
-
 echo "$data" | awk '
-  BEGIN { }
+  BEGIN {
+     RS="<[^>]+>"
+     print "filename\trequirements\tdescription\tF\tP"
+  }
   {
+      gsub(/\n/,"",$0)
       if (/TEST RESULT/)
       {
-          print ; reqs=""
+          patsplit($0, v, ": *", seps)
+          patsplit(seps[1], v, " ", pf)
+          vars[pf[1]]=pf[0]
+          vars[pf[3]]=pf[2]
+          print vars["filename"] "\t" vars["requirement"] "\t" vars["description"] "\t" vars["F"] "\t" vars["P"]
+          delete vars
       }
       else
       {
-          print
+          patsplit(RT, v, "</|>", seps);
+          if (length(seps)>1)
+          {
+              if (seps[1] in vars) { vars[seps[1]] = vars[seps[1]] ", " }
+              vars[seps[1]] = vars[seps[1]] $0
+          }
       }
   }
-  END { print "DONE" }
 '
 
