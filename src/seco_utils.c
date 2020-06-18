5f2c1da9fa21f7946196672e81cb77cf01e8ab5a
Stéphane Dion <stephane.dion_1@nxp.com>
2020-06-18 16:23:42 +0200

    V2XA-310: missing responses header tags for V2X NVM channels
    Signed-off-by: Stéphane Dion <stephane.dion_1@nxp.com>

--- src/seco_utils.c
+++ src/seco_utils.c
@@ -62,6 +62,7 @@ void seco_fill_rsp_msg_hdr(struct sab_mu_hdr *hdr, uint8_t cmd, uint32_t len, ui
         hdr->ver = V2X_SV1_API_VER;
         break;
     case MU_CHANNEL_V2X_SHE:
+    case MU_CHANNEL_V2X_SHE_NVM:
         hdr->tag = V2X_SHE_IND_TAG;
         hdr->ver = V2X_SHE_API_VER;
         break;
@@ -70,6 +71,7 @@ void seco_fill_rsp_msg_hdr(struct sab_mu_hdr *hdr, uint8_t cmd, uint32_t len, ui
         hdr->ver = V2X_SG0_API_VER;
         break;
     case MU_CHANNEL_V2X_SG1:
+    case MU_CHANNEL_V2X_HSM_NVM:
         hdr->tag = V2X_SG1_IND_TAG;
         hdr->ver = V2X_SG1_API_VER;
         break;
