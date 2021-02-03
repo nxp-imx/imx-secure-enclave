b927ff4b37f785b70eea7fa7278d11103bf15a80
Massimiliano Bartoli <massimiliano.bartoli@nxp.com>
2021-02-03 12:26:34 +0100

    HSM-481: hsm_api_document 3.2 amendment
    Signed-off-by: Massimiliano Bartoli <massimiliano.bartoli@nxp.com>

--- include/hsm/hsm_api.h
+++ include/hsm/hsm_api.h
@@ -439,6 +439,10 @@ hsm_err_t hsm_close_key_management_service(hsm_hdl_t key_management_hdl);
  * - \ref HSM_KEY_TYPE_ECDSA_BRAINPOOL_T1_512 is not supported.
  * - \ref HSM_KEY_TYPE_DSA_SM2_FP_256 is not supported.
  * - \ref HSM_KEY_TYPE_SM4_128 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_224 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_256 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_384 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_512 is not supported.
  *
  * - \ref hsm_butterfly_key_expansion: This feature is disabled when part is running in FIPS approved mode. Any call to this API will results in a HSM_FEATURE_DISABLED error.
  * - \ref hsm_key_type_t of op_butt_key_exp_args_t: Only HSM_KEY_TYPE_ECDSA_NIST_P256 and HSM_KEY_TYPE_ECDSA_BRAINPOOL_R1_256 are supported.
@@ -450,6 +454,11 @@ hsm_err_t hsm_close_key_management_service(hsm_hdl_t key_management_hdl);
  *
  * - \ref HSM_OP_MANAGE_KEY_GROUP_FLAGS_DELETE is not supported.
  *
+ * - \ref HSM_KEY_TYPE_HMAC_224 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_256 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_384 is not supported.
+ * - \ref HSM_KEY_TYPE_HMAC_512 is not supported.
+ * 
  * - \ref hsm_key_type_t of op_butt_key_exp_args_t: Only HSM_KEY_TYPE_ECDSA_NIST_P256, HSM_KEY_TYPE_ECDSA_BRAINPOOL_R1_256 and HSM_KEY_TYPE_ECDSA_BRAINPOOL_T1_256 are supported.
  *
  */
@@ -1313,6 +1322,17 @@ hsm_err_t hsm_mac_one_go(hsm_hdl_t mac_hdl, op_mac_one_go_args_t* args, hsm_mac_
 hsm_err_t hsm_close_mac_service(hsm_hdl_t mac_hdl);
 
 /**
+ *\addtogroup qxp_specific
+ * \ref group16
+ *
+ * - \ref HSM_OP_MAC_ONE_GO_ALGO_HMAC_SHA_224 is not supported.
+ * - \ref HSM_OP_MAC_ONE_GO_ALGO_HMAC_SHA_256 is not supported.
+ * - \ref HSM_OP_MAC_ONE_GO_ALGO_HMAC_SHA_384 is not supported.
+ * - \ref HSM_OP_MAC_ONE_GO_ALGO_HMAC_SHA_512 is not supported.
+ *
+ */
+
+/**
  *\addtogroup dxl_specific
  * \ref group16
  *
@@ -1635,11 +1655,8 @@ hsm_err_t hsm_tls_finish(hsm_hdl_t key_management_hdl, op_tls_finish_args_t *arg
 /**
  *\addtogroup qxp_specific
  * \ref group20
- *
- * - \ref HSM_KE_SCHEME_SM2_FP_256 is not supported.
- * - \ref HSM_KDF_ALG_FOR_SM2 is not supported.
- * - \ref HSM_OP_KEY_EXCHANGE_FLAGS_KEY_CONF_EN is not supported
- *
+ * 
+ * \ref hsm_key_exchange API is not supported.
  */
 /**
  *\addtogroup dxl_specific
