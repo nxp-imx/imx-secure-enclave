nxp-imx/imx-secure-enclave

commit 41f834d972fe272b4e82b6422e0f40d6a5562a1b

LF-5914: hsm lib userapp compilation issue
Fix for hsm lib userapp compilation issue.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

Makefile
@@ -14,7 +14,7 @@
 CFLAGS := -O1 -Werror -Wformat -fPIC
 DESTDIR ?= export
 BINDIR ?= /usr/bin
-base_libdir ?= /lib
+base_libdir ?= lib
 LIBDIR ?= /usr/$(base_libdir)
 INCLUDEDIR ?= /usr/include
 PLAT ?= seco
@@ -40,11 +40,12 @@ include $(PLAT_COMMON_PATH)/sab_msg/sab_msg.mk
 include $(PLAT_COMMON_PATH)/hsm_api/hsm_api.mk
 include $(PLAT_PATH)/$(PLAT).mk
 
-PROJECT	:= $(SHE_TEST) $(HSM_TEST) $(V2X_TEST) $(SHE_LIB) $(NVM_LIB) $(HSM_LIB)
+tests: $(SHE_TEST) $(HSM_TEST) $(V2X_TEST)
+libs: $(SHE_LIB) $(NVM_LIB) $(HSM_LIB)
 
-all: ${PROJECT}
+.PHONY: all $(libs) $(tests) clean
 
-.PHONY: all clean
+all: $(libs) $(tests)
 
 %.o: %.c
 	@echo "  HOSTCC  $<"
@@ -97,7 +98,7 @@ $(V2X_TEST): $(V2X_TEST_OBJ) $(HSM_LIB) $(NVM_LIB)
 	$(CC) $^  -o $@ ${INCLUDE_PATHS} $(CFLAGS) -lpthread -lz $(GCOV_FLAGS)
 
 clean:
-	rm -rf $(OBJECTS) *.gcno *.a *_test $(TEST_OBJ) $(DESTDIR)
+	rm -rf $(OBJECTS) *.gcno *.a *_test $(TEST_OBJ)
 
 she_doc: include/she_api.h include/nvm.h
 	rm -rf doc/latex/
@@ -113,8 +114,11 @@ hsm_doc: include/hsm/hsm_api.h
 	cp doc/latex/refman.pdf doc/hsm_api_document.pdf
 	rm -rf doc/latex/
 
-install: $(HSM_TEST) $(SHE_TEST) $(SHE_LIB) $(NVM_LIB) $(HSM_LIB)
-	mkdir -p $(DESTDIR)$(BINDIR) $(DESTDIR)$(LIBDIR) $(DESTDIR)$(INCLUDEDIR)
+install: $(libs)
+	mkdir -p $(DESTDIR)$(LIBDIR) $(DESTDIR)$(INCLUDEDIR)
 	cp $(NVM_LIB) $(HSM_LIB) $(SHE_LIB) $(DESTDIR)$(LIBDIR)
-	cp $(HSM_TEST) $(SHE_TEST) $(DESTDIR)$(BINDIR)
 	cp -a include/* $(DESTDIR)$(INCLUDEDIR)
+
+install_tests: $(install) $(tests)
+	mkdir -p $(DESTDIR)$(BINDIR)
+	cp $(HSM_TEST) $(SHE_TEST) $(DESTDIR)$(BINDIR)

include/hsm/hsm_api.h
@@ -16,9 +16,10 @@
 
 #include <stdint.h>
 
-#include "hsm/internal/hsm_handle.h"
-#include "hsm/internal/hsm_key.h"
-#include "hsm/internal/hsm_utils.h"
+#include "internal/hsm_handle.h"
+#include "internal/hsm_key.h"
+#include "internal/hsm_managekey.h"
+#include "internal/hsm_utils.h"
 
 /**
  *  @defgroup group1 Session

commit fc1f0760385ba734293163dc277f346d0e1105d5

LF-5930 Makefile: fix the target install_tests
LF-5930: fix the target install_tests in Makefile.
Error introduced in commit 41f834d972fe272b4e82b6422e0f40d6a5562a1b.

Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>

Makefile
@@ -119,6 +119,6 @@ install: $(libs)
 	cp $(NVM_LIB) $(HSM_LIB) $(SHE_LIB) $(DESTDIR)$(LIBDIR)
 	cp -a include/* $(DESTDIR)$(INCLUDEDIR)
 
-install_tests: $(install) $(tests)
+install_tests: install $(tests)
 	mkdir -p $(DESTDIR)$(BINDIR)
 	cp $(HSM_TEST) $(SHE_TEST) $(DESTDIR)$(BINDIR)