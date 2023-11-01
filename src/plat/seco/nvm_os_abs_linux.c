0cc2c8f83057852b4afdeff756726eac7079b072
Pankaj Gupta <pankaj.gupta@nxp.com>
2023-11-01 16:04:33 +0530

    LF-10427: nvm: plat_os: create directory as part file writing
    func plat_os_abs_storage_write, is updated to create
    the directory part of the abosulute file-name.
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: Rahul Kumar Yadav <rahulkumar.yadav@nxp.com>

--- src/plat/seco/nvm_os_abs_linux.c
+++ src/plat/seco/nvm_os_abs_linux.c
@@ -19,48 +19,68 @@
 #include "plat_utils.h"
 #include "seco_mu_ioctl.h"
 
+static uint32_t get_dir_name(char *path, char *dir_name)
+{
+	int dir_name_len;
+	uint32_t ret = PLAT_FAILURE;
+	char *last_slash = strrchr(path, '/');
+
+	if (strlen(last_slash)) {
+		dir_name_len = (strlen(path) - strlen(last_slash));
+
+		ret = plat_os_abs_memcpy_v2(dir_name, path, dir_name_len);
+		if (ret != PLAT_SUCCESS)
+			dir_name_len = 0;
+
+		dir_name[dir_name_len] = '\0';
+	}
+	return ret;
+}
+
 /* Write data in a file located in NVM. Return the size of the written data. */
 uint32_t plat_os_abs_storage_write(struct plat_os_abs_hdl *phdl,
 				   uint8_t *src, uint32_t size,
 				   uint8_t *nvm_fname)
 {
 	int32_t fd;
-	int64_t l = 0;
-	char *path;
+	int64_t l;
+	char nvm_storage_dname[256];
 
-	switch (phdl->type) {
-	case MU_CHANNEL_PLAT_SHE_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_PLAT_HSM_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_V2X_SHE_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_V2X_HSM_NVM:
-		path = nvm_fname;
-		break;
-	default:
-		path = NULL;
-		break;
-	}
-	if (path != NULL) {
+	if (phdl->type == MU_CHANNEL_PLAT_SHE_NVM ||
+	    phdl->type == MU_CHANNEL_PLAT_HSM_NVM ||
+	    phdl->type == MU_CHANNEL_V2X_SHE_NVM ||
+	    phdl->type == MU_CHANNEL_V2X_HSM_NVM) {
 		/* Open or create the file with access reserved
 		 * to the current user.
 		 */
-		fd = open(path, O_CREAT|O_WRONLY|O_SYNC, S_IRUSR|S_IWUSR);
+		if (get_dir_name(nvm_fname, nvm_storage_dname)) {
+			l = PLAT_WRITE_FAILURE;
+			goto exit;
+		}
+
+		l = mkdir(nvm_storage_dname, 0x600);
+		if (l < 0 && errno != EEXIST) {
+			l = PLAT_WRITE_FAILURE;
+			goto exit;
+		}
+
+		/* 0x600 for S_IRUSR | S_IWUSR */
+		fd = open(nvm_fname,
+			  O_CREAT | O_WRONLY | O_SYNC,
+			  0x600);
 		if (fd >= 0) {
 			/* Write the data. */
 			l = write(fd, src, size);
 			if (l < 0) {
 				l = PLAT_WRITE_FAILURE;
-				se_err("Write error [%d]:%s\n", errno, strerror(errno));
+				se_err("Write error [%d]:%s\n", errno,
+				       strerror(errno));
 			}
 
 			(void)close(fd);
 		}
 	}
+exit:
 
 	return TO_UINT32_T(l);
 }
@@ -71,29 +91,13 @@ uint32_t plat_os_abs_storage_read(struct plat_os_abs_hdl *phdl,
 {
 	int32_t fd;
 	int64_t l = 0;
-	char *path;
 
-	switch (phdl->type) {
-	case MU_CHANNEL_PLAT_SHE_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_PLAT_HSM_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_V2X_SHE_NVM:
-		path = nvm_fname;
-		break;
-	case MU_CHANNEL_V2X_HSM_NVM:
-		path = nvm_fname;
-		break;
-	default:
-		path = NULL;
-		break;
-	}
-
-	if (path != NULL) {
+	if (phdl->type == MU_CHANNEL_PLAT_SHE_NVM ||
+	    phdl->type == MU_CHANNEL_PLAT_HSM_NVM ||
+	    phdl->type == MU_CHANNEL_V2X_SHE_NVM ||
+	    phdl->type == MU_CHANNEL_V2X_HSM_NVM) {
 		/* Open the file as read only. */
-		fd = open(path, O_RDONLY);
+		fd = open(nvm_fname, O_RDONLY);
 		if (fd >= 0) {
 			/* Read the data. */
 			l = read(fd, dst, size);
@@ -150,7 +154,6 @@ uint32_t get_chunk_file_path(char **path,
 		if (ret != (path_len - 1))
 			ret = 0;
 	}
-
 exit:
 	return (ret <= 0 ? 0u : ret);
 }
@@ -181,13 +184,15 @@ uint32_t plat_os_abs_storage_write_chunk(struct plat_os_abs_hdl *phdl,
 		/* Open or create the file with access reserved
 		 * to the current user.
 		 */
-		fd = open(path, O_CREAT|O_WRONLY|O_SYNC, S_IRUSR|S_IWUSR);
+		/* 0x600 for S_IRUSR | S_IWUSR */
+		fd = open(path, O_CREAT | O_WRONLY | O_SYNC, 0x600);
 		if (fd >= 0) {
 			/* Write the data. */
 			l = write(fd, src, size);
 			if (l < 0) {
 				l = PLAT_WRITE_FAILURE;
-				se_err("Write error [%d]:%s\n", errno, strerror(errno));
+				se_err("chunk-Write error [%d]:%s\n", errno,
+				       strerror(errno));
 			}
 
 			(void)close(fd);
@@ -228,7 +233,8 @@ uint32_t plat_os_abs_storage_read_chunk(struct plat_os_abs_hdl *phdl,
 			l = read(fd, dst, size);
 			if (l < 0) {
 				l = PLAT_READ_FAILURE;
-				se_err("Read error [%d]:%s\n", errno, strerror(errno));
+				se_err("Chunk-Read error [%d]:%s\n", errno,
+				       strerror(errno));
 			}
 
 			(void)close(fd);
